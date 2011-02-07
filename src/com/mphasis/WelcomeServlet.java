a36d40d6da6f0d303b5687e8e3ca8399d178687c
zz9kxh <zz9kxh@w2zz9kxh01.apac.corp.eds.com>
2011-02-07 14:07:29 +0530

    some

---
+++ src/com/mphasis/WelcomeServlet.java
@@ -0,0 +1,72 @@
+package com.mphasis;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		/*
+		* Get the value of form parameter
+		*/
+		String name = request.getParameter("name");
+		String welcomeMessage = "Welcome "+name;
+		/*
+		* Set the content type(MIME Type) of the response.
+		*/
+		response.setContentType("text/html");
+		 
+		/*
+		 * You can do any processing here. 
+		 * We will simply output the value of name parameter on server console.
+		 * 
+		 */
+		System.out.println(name);
+		String destination = "/jsp/result.jsp";
+                 
+		try{
+
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(destination);
+		rd.forward(request, response);
+		System.out.println(name);
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
+
+		 
+
+		
+		
+	}
+
+}
