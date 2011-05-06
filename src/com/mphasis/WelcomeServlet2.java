0f410d91d17f0c457a64b0e4a06d6f7490f8e982
anandbala <bala_anand2004@yahoo.com>
2011-05-06 07:55:49 -0700

    sadsdasd

--- src/com/mphasis/WelcomeServlet2.java
+++ src/com/mphasis/WelcomeServlet2.java
@@ -55,7 +55,7 @@ public class WelcomeServlet1 extends HttpServlet {
 		 * 
 		 */
 		System.out.println(name);
-String destination = "/jsp/resulta353678.jsp";
+String destination = "/jsp/resulta35367fd8.jsp";
                 String origin = ""; 
 		try{
 
